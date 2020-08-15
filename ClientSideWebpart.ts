e5b504778355a5af6525d0e88f77ed0aa034b961
savitej <32020545+savitej@users.noreply.github.com>
2020-08-15 18:55:12 -0400

    Add files via upload

---
+++ ClientSideWebpart.ts
@@ -0,0 +1,17 @@
+public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.clientWebPartExample }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to SharePoint Framework Environment!</span>
+              <p class="${ styles.subTitle }">This is first ClientSide Webpart.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+  }
